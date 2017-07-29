402350325d36b7502e6d601efbd80d39d6051b9c
Pierre <Pierre@169.254.64.225>
2017-07-29 19:00:18 +0200

    Bouton 1à0 fonctionnel

--- js/fonctions.js
+++ js/fonctions.js
@@ -15,6 +15,7 @@ var multiply = document.getElementById('multiply');
 var divide = document.getElementById('divide');
 var less = document.getElementById('less');
 var more = document.getElementById('more');
+var ce = document.getElementById('ce');
 
 
 
@@ -22,8 +23,61 @@ var more = document.getElementById('more');
 //définition des fontions 
 
 function reset() {
-    
+
     result.value = '0';
 }
+function add(num) {
+   
+    if (result.value == 0) {
+        var operante = '';
+    } else {
+        var operante = result.value;
+    }
+var newOperante = operante + num;
+result.value = newOperante; 
+}
 
+//définition des évenement
+
+ce.addEventListener('click', reset);
+num9.addEventListener('click', function()  {
+    add('9');
+}
+);
+num8.addEventListener('click', function()  {
+    add('8');
+}
+);
+num7.addEventListener('click', function()  {
+    add('7');
+}
+);
+num6.addEventListener('click', function()  {
+    add('6');
+}
+);
+num5.addEventListener('click', function()  {
+    add('5');
+}
+);
+num4.addEventListener('click', function()  {
+    add('4');
+}
+);
+num3.addEventListener('click', function()  {
+    add('3');
+}
+);
+num2.addEventListener('click', function()  {
+    add('2');
+}
+);
+num1.addEventListener('click', function()  {
+    add('1');
+}
+);
+num0.addEventListener('click', function()  {
+    add('0');
+}
+);
 
