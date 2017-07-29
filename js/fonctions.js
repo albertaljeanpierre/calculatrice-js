4ddc830f6a72c1d14d5f57bc6d60c5817d0022da
Pierre <Pierre@169.254.64.225>
2017-07-29 17:18:11 +0200

    Deffinition des noms pour chaque bouton (html et JS)

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,3 +1,29 @@
 
+//définition des objets
+var result = document.getElementById('result');
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+var num3 = document.getElementById('num3');
+var num4 = document.getElementById('num4');
+var num5 = document.getElementById('num5');
+var num6 = document.getElementById('num6');
+var num7 = document.getElementById('num7');
+var num8 = document.getElementById('num8');
+var num9 = document.getElementById('num9');
+var num0 = document.getElementById('num0');
+var multiply = document.getElementById('multiply');
+var divide = document.getElementById('divide');
+var less = document.getElementById('less');
+var more = document.getElementById('more');
+
+
+
+
+//définition des fontions 
+
+function reset() {
+    
+    result.value = '0';
+}
 
 
